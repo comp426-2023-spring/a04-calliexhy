b96a401e82ced353b131ac683b54d6285ac36da4
calliexhy <69410421+calliexhy@users.noreply.github.com>
2023-04-24 18:20:58 -0400

    Try

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
+import { rps, rpsls } from './lib/rpsls.js'
 import express from 'express';
 import minimist from 'minimist';
-import { rps, rpsls } from './lib/rpsls.js'
 
 const app = express();
 
@@ -28,10 +28,17 @@ app.get('/app/rps/play/', (req, res) => {
     res.status(200).send(rps(req.query.shot));
 })
 
+app.post('/app/rps/play/', (req, res) => {   
+    res.status(200).send(rps(req.body.shot));
+})
+
 app.get('/app/rpsls/play/', (req, res) => {   
     res.status(200).send(rpsls(req.query.shot));
 })
 
+app.post('/app/rpsls/play/', (req, res) => {   
+    res.status(200).send(rpsls(req.body.shot));
+})
 
 app.get('/app/rps/play/:shot/', (req, res) => {     
     res.status(200).send(rps(req.params.shot));
@@ -41,13 +48,6 @@ app.get('/app/rpsls/play/:shot/', (req, res) => {
     res.status(200).send(rpsls(req.params.shot));
 })
 
-app.post('/app/rps/play/', (req, res) => {   
-    res.status(200).send(rps(req.body.shot));
-})
-
-app.post('/app/rpsls/play/', (req, res) => {   
-    res.status(200).send(rpsls(req.body.shot));
-})
 
 app.get('*', (req, res) => {
     res.status(404).send('404 NOT FOUND');

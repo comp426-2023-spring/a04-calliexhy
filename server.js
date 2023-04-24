a380d95c2cf7fa4fa793f8d33026ba608eb676cc
calliexhy <69410421+calliexhy@users.noreply.github.com>
2023-04-24 18:39:55 -0400

    T

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const PORT = args.port || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-app.get('/app/', (res) => {     
+app.get('/app/', (req, res) => {     
     res.status(200).send("200 OK")
 })
 
-app.get('/app/rps/', (res) => {     
+app.get('/app/rps/', (req, res) => {     
     res.status(200).send(rps(null));
 })
 
@@ -28,27 +28,27 @@ app.get('/app/rps/play/', (req, res) => {
     res.status(200).send(rps(req.query.shot));
 })
 
-app.post('/app/rps/play', (req, res) => {   
+app.post('/app/rps/play/', (req, res) => {   
     res.status(200).send(rps(req.body.shot));
 })
 
-app.get('/app/rpsls/play', (req, res) => {   
+app.get('/app/rpsls/play/', (req, res) => {   
     res.status(200).send(rpsls(req.query.shot));
 })
 
-app.post('/app/rpsls/play', (req, res) => {   
+app.post('/app/rpsls/play/', (req, res) => {   
     res.status(200).send(rpsls(req.body.shot));
 })
 
-app.get('/app/rps/play/:shot', (req, res) => {     
+app.get('/app/rps/play/:shot/', (req, res) => {     
     res.status(200).send(rps(req.params.shot));
 })
 
-app.get('/app/rpsls/play/:shot', (req, res) => {   
+app.get('/app/rpsls/play/:shot/', (req, res) => {   
     res.status(200).send(rpsls(req.params.shot));
 })
 
-app.get('*', ( res) => {
+app.get('*', (req, res) => {
     res.status(404).send('404 NOT FOUND');
   });
 

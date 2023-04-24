72fe7123d54963e7efd594ced57cf688d8a9f388
calliexhy <69410421+calliexhy@users.noreply.github.com>
2023-04-24 18:36:44 -0400

    Fix syntax

--- server.js
+++ server.js
@@ -12,19 +12,19 @@ const PORT = args.port || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-app.get('/app/', (req, res) => {     
+app.get('/app/', (res) => {     
     res.status(200).send("200 OK")
 })
 
-app.get('/app/rps', (req, res) => {     
+app.get('/app/rps/', (res) => {     
     res.status(200).send(rps(null));
 })
 
-app.get('/app/rpsls', (req, res) => {   
+app.get('/app/rpsls/', (res) => {   
     res.status(200).send(rpsls(null));
 })
 
-app.get('/app/rps/play', (req, res) => {   
+app.get('/app/rps/play/', (req, res) => {   
     res.status(200).send(rps(req.query.shot));
 })
 
@@ -48,7 +48,7 @@ app.get('/app/rpsls/play/:shot', (req, res) => {
     res.status(200).send(rpsls(req.params.shot));
 })
 
-app.get('*', (req, res) => {
+app.get('*', ( res) => {
     res.status(404).send('404 NOT FOUND');
   });
 
